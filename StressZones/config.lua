727d43bede850618f27fed42923c178a79f48f9b
Apollomcflurry <91848925+Apollomcflurry@users.noreply.github.com>
2023-05-06 16:20:49 -0400

    Add files via upload
    Script I converted over from Fivem

---
+++ StressZones/config.lua
@@ -0,0 +1,24 @@
+Config = {}
+
+-- ╔════╗  ╔══════ ╔════╗   ╔══════ ╔╗   ║ ╔══════
+-- ║    ╚╗ ║       ║    ╚╗  ║       ║╚╗  ║ ║
+-- ║     ║ ╠═══    ╠╦════╝  ╠═══    ║ ╚╗ ║ ╚═════╗
+-- ║    ╔╝ ║       ║╚═╗     ║       ║  ╚╗║       ║
+-- ╚════╝  ╚══════ ║  ╚═╗	╚══════ ║   ╚╝ ══════╝
+-- Version 1.0 
+
+
+-- Sets the distance around where the antistress effect will be activated. DEFAULT = 20
+Config.distanciaZona = 20
+
+-- Set the amount of stress you want to eliminate. DEFAULT QB = 1
+Config.cantidadStressReducir = 600
+
+-- Sets the time it takes for the plugin to reduce stress "in seconds". DEFAULT = 10
+Config.segundosDelay = 10
+
+-- Set anti-stress zones
+Config.stressZones = {
+	-- Gym
+	vector3(-305.6642, 806.47857, 118.98025)
+}
